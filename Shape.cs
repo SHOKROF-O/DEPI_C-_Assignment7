eeabb6c1a0c7423b5e9badb1197eb117c1e51dee
SHOKROF <libyaomar996@gmail.com>
2026-09-17 22:46:32 +0300

    Add Shape

---
+++ Shape.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Assignment_03
+{
+    public class Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Shape(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public override string ToString()
+        {
+            return $"(Width = {Width}, Height = {Height})";
+        }
+    }
+}
