a64e1981a4bfca962fbda4db9ba6a501d2554bd8
SHOKROF <libyaomar996@gmail.com>
2026-09-17 22:47:31 +0300

    Add Cube

---
+++ Cube.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Assignment_03
+{
+    public class Cube : Shape
+    {
+        public double Depth { get; set; }
+
+        public Cube(double width, double height, double depth) : base(width, height)
+        {
+            Depth = depth;
+        }
+
+        public new double Area()
+        {
+            return base.Area() * Depth;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Width = {Width}, Height = {Height}, Depth = {Depth}");
+        }
+    }
+}
